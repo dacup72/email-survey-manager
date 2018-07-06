14cd9d5d1fd0698214c1951d50af67f0a79ffdf0
dacup72 <dacup72@gmail.com>
2018-07-06 14:56:16 -0600

    Testing out relative route for google Oauth

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,9 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">Logo</a>
+          <a href="#" className="left brand-logo">Email Survey Manager</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="sass.html">Sass</a></li>
-            <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li>
+            <li><a href="/auth/google">Login With Google</a></li>
           </ul>
         </div>
       </nav>
