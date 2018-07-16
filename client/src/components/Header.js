d65485ef3e520d078c883cdd2d16c8057594cf4e
dacup72 <dacup72@gmail.com>
2018-07-16 14:41:39 -0600

    Added logic to Header component for determining user login status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  // Determines content to show based on users login status
+  renderContent() {
+    switch (this.props.auth) {
+      case null: 
+      return 'Still deciding';
+
+      case false:;
+      return 'Im logged out';
+
+      default: 
+        return 'Im logged in';
+      
+    }
+  }
+
   render() {
+    // this.props contains the users login object
+    // console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Email Survey Manager</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/auth/google">Login With Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,6 +33,9 @@ class Header extends Component {
   }
 }
 
+// Passes the state to the Header object as props
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
 
-
-export default Header
+export default connect(mapStateToProps)(Header);
