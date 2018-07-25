df8e7e9643d355c1a2275762dedd0fc0605b2494
dacup72 <dacup72@gmail.com>
2018-07-24 21:53:11 -0600

    Added sub document collection

---
+++ models/Recipient.js
@@ -0,0 +1,14 @@
+// SUBDOCUMENT COLLECTION
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const recipientSchema = new Schema({
+  email: String,
+  responded: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = recipientSchema;
--- models/Survey.js
+++ models/Survey.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
   title: String,
   subject: String,
   body: String,
-  // Array containing list of strings
+  // Array containing list of recipient schemas
   // Stores "sub document collection"
-  recipients: [String],
+  recipients: [RecipientSchema],
   yes: {
     type: Number,
     default: 0
