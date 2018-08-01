20c8fb6348897e90916f924eca002ef97b1a0bd9
dacup72 <dacup72@gmail.com>
2018-08-01 16:25:43 -0600

    stuff

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,20 +7,21 @@ const requireCredits = require("../middlewares/requireCredits");
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  // Post route for sending surveys
+  // Post route for creating new surveys sending big emails with those surveys
   // Make sure user is logged in (functions in the line of the post request are envoked in order)
   // Make sure user has enough credits
   app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
     // ES6 destructuring to obtain differenty properties from an object
     const { title, subject, body, recipients } = req.body;
 
+    // create new Survey model object for creating and send new emails
     const survey = new Survey({
       // ES6 syntax for defining "exact same" named key value pairs
       title,
       subject,
       body,
-      // Takes comma delimited string of emails, splits it into array, and returns array of email objects
-      recipients: recipients.split(",").map(email => ({ email })),
+      // Takes comma delimited string of emails, splits it into array, and returns array of email objects containing white space trimed email addresses
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
       // Passes in mongooses automatically generated id for the User model 
       _user: req.user.id,
       dateSent: Date.now()
