e48a9ab19584aced2889c98de83d19f01dc6d8a4
dacup72 <dacup72@gmail.com>
2018-08-05 17:09:10 -0600

    Fixed email validator

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -2,12 +2,15 @@
 const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default emails => {
-  // TODO: make it so that the last comma does not throw invalid message to user
   
   const invalidEmails = emails
     .split(',')
     .map(email => email.trim())
     .filter(email => re.test(email) === false);
+  
+  if(!invalidEmails[invalidEmails.length -1]) {
+    invalidEmails.splice(invalidEmails.length - 1, 1);
+  }
   
   if (invalidEmails.length) {
     return `These emails are invalid ${invalidEmails}`;
