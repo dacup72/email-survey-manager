900429c0affa5a35b1b582b3a72fbb4b77f47b00
dacup72 <dacup72@gmail.com>
2018-08-05 13:15:52 -0600

    Connected survey routes to database

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,12 +11,17 @@ module.exports = app => {
   // Post route for creating new surveys sending big emails with those surveys
   // Make sure user is logged in (functions in the line of the post request are envoked in order)
   // Make sure user has enough credits
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     // ES6 destructuring to obtain differenty properties from an object
-    const { title, subject, body, recipients } = req.body;
-      // Examples:
-      // const title = req.body.title;
-      // const subject = req.body.subject;
+    const {
+      title,
+      subject,
+      body,
+      recipients
+    } = req.body;
+    // Examples:
+    // const title = req.body.title;
+    // const subject = req.body.subject;
 
     // create new Survey model object for creating and send new emails
     const survey = new Survey({
@@ -25,7 +30,9 @@ module.exports = app => {
       subject,
       body,
       // Takes comma delimited string of emails, splits it into array, and returns array of email objects containing white space trimed email addresses
-      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
+      recipients: recipients.split(",").map(email => ({
+        email: email.trim()
+      })),
       // Passes in mongooses automatically generated id for the User model 
       _user: req.user.id,
       dateSent: Date.now()
@@ -34,6 +41,18 @@ module.exports = app => {
 
     // Send emails to sendGrid
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+
+      req.user.credits -= 1;
+      const user = await req.user.save();
+
+      res.send(user);
+    } catch (err) {
+      // status 422 means unproccessable entity (something went wrong with the data you sent us)
+      res.status(422).send(err);
+    }
   });
 };
