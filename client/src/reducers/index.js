ca748869e0cd534f6ce16efa02f8d0216f0fb724
dacup72 <dacup72@gmail.com>
2018-08-05 14:35:20 -0600

    added redux-form package

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+// Rename the import with "as"
+import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
 
 // This is the model for the Redux global state
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: reduxForm
 });
