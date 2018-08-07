c1c372a45e66f5ccfeda4e83a0c9804d7133908c
dacup72 <dacup72@gmail.com>
2018-08-06 18:12:45 -0600

    small fix

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 // Rename the import with "as"
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
-import surveyReducer from './surveysReducer';
+import surveysReducer from './surveysReducer';
 
 // This is the model for the Redux global state
 export default combineReducers({
   auth: authReducer,
   form: reduxForm,
-  surveys: surveyReducer
+  surveys: surveysReducer
 });
