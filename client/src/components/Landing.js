25f8aca0824e27549bbfbfee0805c45d8f2e4661
dacup72 <dacup72@gmail.com>
2018-08-10 16:07:14 -0500

    Updated landing page with login button

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
 
-const Landing = () => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Dashboard</h1>
-      Collect feedback from your users
-    </div>
-  );
+class Landing extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        // returns nothing if null
+        return;
+
+      case false:
+        return <a href="/auth/google">Login With Google</a>;
+
+      default: 
+        return <Link to="/surveys">Create Surveys</Link>;
+    }
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Email Survey Manager</h1>
+        Collect feedback from your users
+        <hr/>
+        {this.renderContent()}
+      </div>
+    );
+  }
 };
 
-export default Landing;
+
+// Passes the state to the Header object as props
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Landing);
